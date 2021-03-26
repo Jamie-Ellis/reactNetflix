12890bcab93c6edeb6d05cfbad613fb37d288957
Ekow <ewusiessel@gmail.com>
2021-03-26 12:26:17 +0000

    loaded images

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-import './components/NavBar'
-import './components/Home'
-import './components/Footer'
+import "./App.css";
+import "./components/NavBar";
+import Home from "./components/Home";
+import "./components/Footer";
+
 function App() {
   return (
     <>
+      <div>
+        <Home />
+      </div>
     </>
   );
 }
