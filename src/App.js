f86e64d1f910375ae4b7f33813745a25e5fcdb0f
Silviu Alexandru Murgu <silviu.murgu@yahoo.com>
2021-03-26 15:59:16 +0200

    added footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import logo from './logo.svg';
+
 import './App.css';
+import './style.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './components/NavBar'
 import './components/Home'
 import './components/Footer'
+import Footer from './components/Footer';
 function App() {
   return (
     <>
+      <Footer />
     </>
   );
 }
