e5fb3e74ca3bce431fdeb28265788adb299d61cd
Jamie <jamiekyaellis@gmail.com>
2021-03-26 17:07:54 +0200

    up

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
 import './components/NavBar'
-import './components/Home'
+import Home from './components/Home'
 import './components/Footer'
 function App() {
   return (
     <>
+    <Home/>
     </>
   );
 }
